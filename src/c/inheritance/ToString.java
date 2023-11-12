3feae1c62f90aa0316c0620a5f654b2fcd7a1696
sanghoon <nonsleep79@gmail.com>
2023-11-12 14:20:11 +0900

    init

---
+++ src/c/inheritance/ToString.java
@@ -0,0 +1,14 @@
+package c.inheritance;
+
+public class ToString {
+    public static void main(String[] args) {
+        ToString thisObject = new ToString();
+        thisObject.toStringMethod(thisObject);
+    }
+
+    private void toStringMethod(Object obj) {
+        System.out.println(obj);
+        System.out.println(obj.toString());
+        System.out.println("plus "+obj);
+    }
+}
